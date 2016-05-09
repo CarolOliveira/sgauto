52c9cc9c3c0163dfed90234d5bdf862da8a16c71
adspgx10 <marcosantonio28@aol.com>
2016-05-09 11:27:46 -0300

    Scripts Added - Marcos

--- index.php
+++ index.php
@@ -1,17 +1,43 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
+<?php
+include 'Includes/conecta.php';
+?>
+<html lang="pt-br">
     <head>
-        <meta charset="UTF-8">
-        <title></title>
+        <title>SGAUTO - Sistema de Gerenciamento para Auto Escola</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="Scripts/css/bootstrap.css">
+        <link rel="stylesheet" type="text/css" href="Scripts/css/style.css">
     </head>
-    <body>
-        <?php
-        // put your code here
-        ?>
+    <?php
+include './Controllers/logica_usuario.php';
+
+if(isset($_SESSION["success"])){?>
+	<p class="alert-success"><?=$_SESSION["success"]?></p>
+<?php
+	unset($_SESSION["success"]);
+}
+
+if(isset($_SESSION["danger"])){?>
+	<p class="alert-danger"><?=$_SESSION["danger"]?> </p>
+<?php
+	unset($_SESSION["danger"]);
+}
+?>
+    
+    <body class="login">
+    <fieldset id="acesso">
+        <h1>Login</h1>
+    <br/><br/>
+    <form action="Models/login.php" method="post">       
+            <table border="0" class="table">
+                <tr><td><font color="black">Login</font></td><td><input type="text" name="login" class="form-control" id="form_login" placeholder="Seu nome de usuário"></td></tr>
+                <tr><td><font color="black">Senha</font></td><td><input type="password" name="senha" class="form-control" id="form_login" placeholder="Sua senha de acesso"></td></tr>
+                <tr><td colspan="2"><div align="right"><button class="btn btn-primary">Logar</button></div></td></tr>
+            </table>
+        </fieldset>    
+    </form>
     </body>
 </html>
+<?php
+require 'Includes/rodape.php';
+?>
