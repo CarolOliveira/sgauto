58f56e4983d2bb3f696cfac07a3a8df9eed47c60
adspgx10 <marcosantonio28@aol.com>
2016-05-10 14:27:51 -0300

    Modificado - Marcos

--- Views/acesso.php
+++ Views/acesso.php
@@ -36,6 +36,7 @@ usuarioEstaLogado();
 <p class="text-success">Você está logado como <u><?=$_SESSION['UsuarioNome']?></u></p><br><br>
 
     <a href="listar_usuario.php"><button class="btn btn-primary" title="CRUD Usuário">Usuário</button></a> 
+    <a href="listar_instrutor.php"><button class="btn btn-primary" title="CRUD Instrutor">Instrutor</button></a>
     <a href="listar_aluno.php"><button class="btn btn-primary" title="CRUD Aluno">Aluno</button></a> 
     <a href="listar_veiculo.php"><button class="btn btn-primary" title="CRUD Veículo">Veículo</button></a> 
     <a href="sair.php"><button class="btn btn-danger" title="Sair do Sistema">Sair</button></a> 
