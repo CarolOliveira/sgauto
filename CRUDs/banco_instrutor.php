ca662dbc3903786caa5e45d9c50f10ddc0881bf1
adspgx10 <marcosantonio28@aol.com>
2016-05-10 12:37:38 -0300

    Modificado - Marcos

--- CRUDs/banco_instrutor.php
+++ CRUDs/banco_instrutor.php
@@ -1,29 +1,29 @@
 <?php
 function insereInstrutor($conexao, $nome, $cpf, $rg, 
         $nacto, $endereco, $cidade, $estado, $tel, $cel, $email){
-    $query = "insert into instrutor (cpf_instrutor, nm_instrutor, dt_nascimento_aluno, "
-            . "cd_registro_geral_aluno, nm_endereco_aluno, "
-            . "nm_cidade_aluno, sg_estado_aluno, "
-            . "cd_telefone_um_aluno, "
-            . "cd_celular_aluno,"
-            . "nm_email_cliente) VALUES('{$cpf}','{$nome}',"
+    $query = "insert into instrutor (cpf_instrutor, nm_instrutor, dt_nascimento_instrutor, "
+            . "cd_registro_geral_instrutor, nm_endereco_instrutor, "
+            . "nm_cidade_instrutor, sg_estado_instrutor, "
+            . "cd_telefone_um_instrutor, "
+            . "cd_telefone_dois_instrutor,"
+            . "nm_email_instrutor) VALUES('{$cpf}','{$nome}',"
             . "'{$nacto}','{$rg}','{$endereco}','{$cidade}',"
             . "'{$estado}','{$tel}','{$cel}','{$email}')";
     return mysqli_query($conexao,$query);
 }
 
-function listarAlunos($conexao){
-	$alunos = array();
-	$query = "select * from aluno order by nm_aluno";
+function listarInstrutores($conexao){
+	$instrutores = array();
+	$query = "select * from instrutor order by nm_instrutor";
 	$resultado = mysqli_query($conexao, $query);
-	while($aluno = mysqli_fetch_assoc($resultado)){
-            array_push($alunos, $aluno);
+	while($instrutor = mysqli_fetch_assoc($resultado)){
+            array_push($instrutores, $instrutor);
 	}
-	return $alunos;
+	return $instrutores;
 }
 
 function buscaInstrutor($conexao, $id){
-	$query = "select * from aluno where cpf_instrutor = {$id}";
+	$query = "select * from instrutor where cpf_instrutor = {$id}";
 	$resultado = mysqli_query($conexao, $query);
 	return mysqli_fetch_assoc($resultado);
 }
@@ -34,7 +34,7 @@ function alterarInstrutor($conexao, $nome, $cpf, $rg, $data, $endereco,
         . "dt_nascimento_instrutor='{$data}',cd_registro_geral_instrutor='{$rg}',"
         . "nm_endereco_instrutor='{$endereco}',nm_cidade_instrutor='{$cidade}',"
         . "sg_estado_instrutor='{$estado}',cd_telefone_um_instrutor='{$tel}',"
-        . "cd_celular_instrutor='{$cel}',nm_email_instrutor='{$email}' "
+        . "cd_telefone_dois_instrutor='{$cel}',nm_email_instrutor='{$email}' "
         . "where cpf_instrutor = '{$cpf}'";
 	return mysqli_query($conexao, $query);
 }
