cadf36c651df1478e978f142d5e3d199c54ccdc2
adspgx10 <marcosantonio28@aol.com>
2016-05-10 12:54:48 -0300

    Modificado - Marcos

--- Controllers/logica_usuario.php
+++ Controllers/logica_usuario.php
@@ -26,6 +26,9 @@ if (!isset($_SESSION["UsuarioID"]) or $_SESSION["UsuarioTipo"] <= 0){
 	$_SESSION["danger"] = "Somente ADM pode ter acesso a página";
 	// Redireciona o visitante de volta pro login
 	header("Location: ../index.php"); exit;
+        }else if($_SESSION['UsuarioTipo'] == 1){
+            header("Location: acesso.php");
+	//Redireciona para nivel de gerente
 }else if($_SESSION['UsuarioTipo'] == 2){
 	//Redireciona para nivel de gerente
 	header("Location: gerente.php");
